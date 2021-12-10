67e732a7044eb66b88494166ace6ebb0dec2ba59
Neoarya <neoarya2012@gmail.com>
2021-12-10 12:43:29 +0530

    implemented global fileOrganizer module

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+
+//help fn done
+function helpFn(dirPath){
+    console.log(`
+    List of all the commands:
+
+    1. node main.js tree "directoryPath"
+    2. node main.js organize "directoryPath"
+    3. node main.js help
+    `);
+}
+
+module.exports = {
+    helpKey: helpFn
+}
